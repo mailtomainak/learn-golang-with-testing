ae7b84130be192f641509d4b2f0e49e34a201b4d
Mainak Chakrabarty <mailtomainak@gmail.com>
2020-05-20 22:21:51 +0200

    Add Table Driven Tests

--- Structs-Methods-Interfaces/shapes_test.go
+++ Structs-Methods-Interfaces/shapes_test.go
@@ -25,4 +25,21 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, 314.1592653589793)
 	})
 
+	areaTests := []struct {
+		name  string
+		shape Shape
+		want  float64
+	}{{"Rectangle", Rectangle{12, 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793}}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
+			}
+
+		})
+	}
+
 }
